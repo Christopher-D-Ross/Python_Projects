ad4b76e2c16df5d613119313b524c10fca265c5f
Christopher-D-Ross <ross758@gmail.com>
2021-08-31 21:40:01 -0400

    Added new interactive Customer Order method to add items to the customer order. This method will also add the price of that item to their total price or total amount owed. This method will run along with other methods when the welcome_cust method is called.

--- resta_prog.py
+++ resta_prog.py
@@ -19,10 +19,11 @@ class Beast:
         self.add_customer(wel_cust["name"], wel_cust["email"], wel_cust["phone"])
         self.print_menu()
         self.add_funds()
+        self.add_to_cust_order()
 
     #Add funds to a Beast prepaid card
     def add_funds(self):
-        fund_cust = input("Would you like to add funds to a prepaid card?")
+        fund_cust = input("Would you like to add funds to your Beast prepaid card?")
         if fund_cust == "yes" or fund_cust == "Yes":
             x = input("How much would you like to add?")
             self.funds += float(x)
@@ -48,6 +49,26 @@ class Beast:
     def remove_items_from_dinner_menu(self, item):
         del self.dinner_menu[item]
 
+    #Customer Order
+    def add_to_cust_order(self):
+        cust_item = input("Please choose an item on the menu to add to your order by entering that item.")
+        if cust_item == "chicken" or cust_item == "Chicken":
+            self.cust_order.append("chicken")
+            self.total_price += 20
+            print("You ordered", self.cust_order,"and your current total is $",self.total_price,".")
+        if cust_item == "broccoli" or cust_item == "Broccoli":
+            self.cust_order.append("broccoli")
+            self.total_price += 10
+            print("You ordered", self.cust_order,"and your current total is $",self.total_price,".")
+        if cust_item == "carrots" or cust_item == "Carrots":
+            self.cust_order.append("carrots")
+            self.total_price += 8
+            print("You ordered", self.cust_order,"and your current total is $",self.total_price,".")
+        if cust_item == "kale" or cust_item == "Kale":
+            self.cust_order.append("kale")
+            self.total_price += 10
+            print("You ordered", self.cust_order,"and your current total is $",self.total_price,".")
+
     #Pricing methods
     def change_item_price(self, existing_item, price):
         self.dinner_menu[existing_item] = price
@@ -57,10 +78,11 @@ class Beast:
 
 set_1 = Beast()
 
-#set_1.welcome_cust()
+set_1.welcome_cust()
 #set_1.add_items_to_dinner_menu("cashews", "8")
 #print(Beast.dinner_menu)
 #set_1.add_funds()
-set_1.remove_items_from_dinner_menu("chicken")
-set_1.change_item_price("broccoli", "15")
-print(Beast.dinner_menu)
+#set_1.remove_items_from_dinner_menu("chicken")
+#set_1.change_item_price("broccoli", "15")
+#print(Beast.dinner_menu)
+#set_1.add_to_cust_order()
